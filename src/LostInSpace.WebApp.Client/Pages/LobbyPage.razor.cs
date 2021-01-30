c6ee4c0e1d515871b4d27200f931900ea7202c52
fydar <fydar.dev@gmail.com>
2021-01-30 04:54:13 +0000

    🚧 Added web client code connect to the server

---
+++ src/LostInSpace.WebApp.Client/Pages/LobbyPage.razor.cs
@@ -0,0 +1,36 @@
+﻿using HuskyNet.WebClient.Services;
+using LostInSpace.WebApp.Shared.Procedures;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using System;
+using System.Threading.Tasks;
+
+namespace LostInSpace.WebApp.Client.Pages
+{
+	public partial class LobbyPage : ComponentBase, IDisposable
+	{
+		[Inject] protected ClientService ClientService { get; set; }
+		[Inject] protected NavigationManager NavigationManager { get; set; }
+		[Inject] protected IJSRuntime JsRuntime { get; set; }
+
+		protected override async Task OnInitializedAsync()
+		{
+			ClientService.OnProcedureApplied += OnProcedureApplied;
+		}
+
+		public void Dispose()
+		{
+			ClientService.OnProcedureApplied -= OnProcedureApplied;
+		}
+
+		public void OnProcedureApplied(NetworkedViewProcedure networkedViewProcedure)
+		{
+			_ = InvokeAsync(StateHasChanged);
+
+			if (ClientService.View?.Lobby?.World != null)
+			{
+				NavigationManager.NavigateTo("/navigate");
+			}
+		}
+	}
+}
---
+++ src/LostInSpace.WebApp.Client/Services/ClientService.cs
@@ -0,0 +1,110 @@
+﻿using Husky.Game.Shared.Commands;
+using Husky.Game.Shared.Model;
+using LostInSpace.WebApp.Shared.Procedures;
+using LostInSpace.WebApp.Shared.Services.Network;
+using LostInSpace.WebApp.Shared.View;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace HuskyNet.WebClient.Services
+{
+	public class ClientService
+	{
+		private readonly ILogger logger;
+		private readonly JsonSerializer serializer;
+		private WebSocketChannel serverConnection;
+
+		public ClientNetworkedView View { get; private set; }
+
+		public event Action<NetworkedViewProcedure> OnProcedureApplied;
+
+		public ClientService(ILogger<ClientService> logger)
+		{
+			this.logger = logger;
+
+			serializer = new JsonSerializer();
+
+			_ = ConnectAsync("wss://localhost:5001");
+		}
+
+		public async Task ConnectAsync(string serverUrl)
+		{
+			try
+			{
+				var target = new Uri($"{serverUrl}/api/game");
+
+				var clientView = new ClientNetworkedView();
+
+				// Client
+				serverConnection = new WebSocketChannel();
+				serverConnection.Logging.OnComplete += log =>
+				{
+					if (log.Level >= LostInSpace.WebApp.Shared.Services.Network.LogLevel.Information)
+					{
+						logger.LogInformation(log.ToString());
+					}
+				};
+				serverConnection.OnReceive += message =>
+				{
+					// Console.WriteLine($"Recived: {Encoding.UTF8.GetString(message)}");
+
+					var procedure = DeserializeProcedure(message.Content);
+					procedure.ApplyToView(clientView);
+
+					OnProcedureApplied?.Invoke(procedure);
+				};
+
+				logger.LogInformation($"Connecting to {target}");
+				await serverConnection.Connect(target);
+
+				if (serverConnection.IsConnected)
+				{
+					logger.LogInformation($"Connected to {target}");
+				}
+				else
+				{
+					logger.LogInformation($"Failed to connect to {target}");
+				}
+				OnProcedureApplied?.Invoke(null);
+
+				View = clientView;
+			}
+			catch (Exception exception)
+			{
+				Console.WriteLine(exception);
+			}
+		}
+
+		public Task SendCommandAsync(INetworkChannel channel, ClientCommand command)
+		{
+			byte[] data = SerializeCommand(command);
+
+			return channel.SendAsync(data);
+		}
+
+		private byte[] SerializeCommand(ClientCommand command)
+		{
+			var serialized = PackagedModel<ClientCommand>.Serialize(command);
+
+			using var memoryStream = new MemoryStream();
+			using var streamWriter = new StreamWriter(memoryStream);
+			using var jsonWriter = new JsonTextWriter(streamWriter);
+
+			serializer.Serialize(jsonWriter, serialized);
+			jsonWriter.Flush();
+			return memoryStream.ToArray();
+		}
+
+		private NetworkedViewProcedure DeserializeProcedure(Stream data)
+		{
+			using var sr = new StreamReader(data);
+			using var jr = new JsonTextReader(sr);
+
+			var deserialized = serializer.Deserialize<PackagedModel<NetworkedViewProcedure>>(jr);
+			return deserialized.Deserialize();
+		}
+	}
+}
