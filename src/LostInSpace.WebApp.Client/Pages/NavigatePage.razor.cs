27d0344c67b699317033daf21ac03b8ab2277d48
Anthony Marmont <dev.anthonymarmont@gmail.com>
2021-01-30 04:54:13 +0000

    🚧 Added web client code connect to the server

---
+++ src/LostInSpace.WebApp.Client/Pages/NavigatePage.razor.cs
@@ -0,0 +1,35 @@
+﻿using HuskyNet.WebClient.Services;
+using LostInSpace.WebApp.Shared.Procedures;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.ComponentModel;
+using System.Threading.Tasks;
+
+namespace LostInSpace.WebApp.Client.Pages
+{
+	public partial class NavigatePage : ComponentBase, IDisposable
+	{
+		[Inject] protected ClientService ClientService { get; set; }
+		[Inject] protected NavigationManager NavigationManager { get; set; }
+
+		protected override Task OnInitializedAsync()
+		{
+			ClientService.OnProcedureApplied += OnProcedureApplied;
+			return Task.CompletedTask;
+		}
+
+		public void ClickConnectButton()
+		{
+		}
+
+		public void Dispose()
+		{
+			ClientService.OnProcedureApplied -= OnProcedureApplied;
+		}
+
+		public void OnProcedureApplied(NetworkedViewProcedure networkedViewProcedure)
+		{
+			_ = InvokeAsync(StateHasChanged);
+		}
+	}
+}
