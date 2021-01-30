4bdc94cc4f91786d9db093a6db951bb8150c9433
Anthony Marmont <dev.anthonymarmont@gmail.com>
2021-01-30 04:49:30 +0000

    ✨ Added shared WebSocket networking scripts

---
+++ src/LostInSpace.WebApp.Shared/Services/Network/INetworkChannel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LostInSpace.WebApp.Shared.Services.Network
+{
+	public interface INetworkChannel
+	{
+		event Action OnConnected;
+
+		event Action<NetworkChannelMessage> OnReceive;
+
+		bool IsConnected { get; }
+
+		Task CloseAsync(CancellationToken cancellationToken = default);
+
+		Task SendAsync(byte[] message, CancellationToken cancellationToken = default);
+	}
+}
---
+++ src/LostInSpace.WebApp.Shared/Services/Network/NetworkLogChannel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace LostInSpace.WebApp.Shared.Services.Network
+{
+	public class NetworkLogChannel
+	{
+		public event Action<NetworkLog> OnStart;
+		public event Action<NetworkLog> OnComplete;
+
+		public NetworkLog Start(string name)
+		{
+			var log = new NetworkLog(this, name).Start();
+
+			OnStart?.Invoke(log);
+
+			return log;
+		}
+
+		internal void InvokeOnComplete(NetworkLog log)
+		{
+			OnComplete?.Invoke(log);
+		}
+	}
+}
